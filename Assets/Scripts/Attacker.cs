0951cca671c3e53944a812d4b87abceb69b86e37
SomeRussianDude <yellow.mockingbird@gmail.com>
2021-12-09 19:59:40 +0100

    Added an animated mob and an npc that spawns coins

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range (0, 5f)][SerializeField] private float walkSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+    }
+
+}
