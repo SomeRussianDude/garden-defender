c00ef02696e7aaacdf0ebe374a3bc9a2a9f45c65
SomeRussianDude <yellow.mockingbird@gmail.com>
2021-12-13 14:08:30 +0100

    Renamed defender script into "Shooter" to better match the purpose

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,8 +1,8 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Defender : MonoBehaviour
+public class Shooter: MonoBehaviour
 {
     [SerializeField] private GameObject projectile, gun;
     
