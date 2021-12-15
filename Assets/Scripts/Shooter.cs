fe94a89ffec0c9f559f36470cfa8a6eb2ecaf1b3
SomeRussianDude <yellow.mockingbird@gmail.com>
2021-12-15 15:54:00 +0100

    Defenders in their Shooter scripts check if there are enemies in their lane (so far only results in logging).

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,11 +1,55 @@
- using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
-public class Shooter: MonoBehaviour
+public class Shooter : MonoBehaviour
 {
     [SerializeField] private GameObject projectile, gun;
-    
+    private AttackerSpawner myLaneSpawner;
+
+    private void Start()
+    {
+        SetSpawnerLane();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("SHOOT!!");
+        }
+        else
+        {
+            Debug.Log("Hold it..........");
+        }
+    }
+
+    private void SetSpawnerLane()
+    {
+        var attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+        foreach (AttackerSpawner spawner in attackerSpawners)
+        {
+            bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
     private void Shoot()
     {
