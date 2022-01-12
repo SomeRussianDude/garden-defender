4231b539b95b4f6c72b209d2a2746114c81a4d45
SomeRussianDude <yellow.mockingbird@gmail.com>
2022-01-12 12:58:31 +0100

    The Defenders' cost is now displayed under their respective buttons

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -2,13 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour
 {
-
     [SerializeField] private Defender defenderPrefab;
-    
-    
+
+    private void Start()
+    {
+        DisplayCost();
+    }
+
+    private void DisplayCost()
+    {
+        Text costText = GetComponentInChildren<Text>();
+        if (!costText)
+        {
+            Debug.LogError($"{name} has no cost text!");
+        }
+        else
+        {
+            costText.text = defenderPrefab.CoinCost.ToString();
+        }
+    }
+
     private void OnMouseDown()
     {
         ButtonSelected();
